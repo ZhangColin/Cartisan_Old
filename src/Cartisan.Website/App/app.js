97f0d475d5e78852474b27e5f59e5e3829d0a619
文野 <stwyhm@126.com>
2015-11-19 18:45:16 +0800

    Metronic app.js

--- src/Cartisan.Website/App/app.js
+++ src/Cartisan.Website/App/app.js
@@ -34,14 +34,78 @@ cartisanApp.factory('settings', ['$rootScope', function ($rootScope) {
     return settings;
 }]);
 
+
+END: BREAKING CHANGE in AngularJS v1.3.x:
+*********************************************/
+
+/* Setup global settings */
+MetronicApp.factory('settings', ['$rootScope', function($rootScope) {
+    // supported languages
+    var settings = {
+        layout: {
+            pageSidebarClosed: false, // sidebar menu state
+            pageBodySolid: false, // solid body color state
+            pageAutoScrollOnLoad: 1000 // auto scroll to top on page load
+        },
+        layoutImgPath: Metronic.getAssetsPath() + 'admin/layout/img/',
+        layoutCssPath: Metronic.getAssetsPath() + 'admin/layout/css/'
+    };
+
+    $rootScope.settings = settings;
+
+    return settings;
+}]);
+
 /* Setup App Main Controller */
-cartisanApp.controller('AppController', ['$scope', '$rootScope', function ($scope, $rootScope) {
-    $scope.$on('$viewContentLoaded', function () {
+MetronicApp.controller('AppController', ['$scope', '$rootScope', function($scope, $rootScope) {
+    $scope.$on('$viewContentLoaded', function() {
         Metronic.initComponents(); // init core components
         //Layout.init(); //  Init entire layout(header, footer, sidebar, etc) on page load if the partials included in server side instead of loading with ng-include directive 
     });
 }]);
 
+/***
+Layout Partials.
+By default the partials are loaded through AngularJS ng-include directive. In case they loaded in server side(e.g: PHP include function) then below partial 
+initialization can be disabled and Layout.init() should be called on page load complete as explained above.
+***/
+
+/* Setup Layout Part - Header */
+MetronicApp.controller('HeaderController', ['$scope', function($scope) {
+    $scope.$on('$includeContentLoaded', function() {
+        Layout.initHeader(); // init header
+    });
+}]);
+
+/* Setup Layout Part - Sidebar */
+MetronicApp.controller('SidebarController', ['$scope', function($scope) {
+    $scope.$on('$includeContentLoaded', function() {
+        Layout.initSidebar(); // init sidebar
+    });
+}]);
+
+/* Setup Layout Part - Quick Sidebar */
+MetronicApp.controller('QuickSidebarController', ['$scope', function($scope) {    
+    $scope.$on('$includeContentLoaded', function() {
+        setTimeout(function(){
+            QuickSidebar.init(); // init quick sidebar        
+        }, 2000)
+    });
+}]);
+
+/* Setup Layout Part - Theme Panel */
+MetronicApp.controller('ThemePanelController', ['$scope', function($scope) {    
+    $scope.$on('$includeContentLoaded', function() {
+        Demo.init(); // init theme panel
+    });
+}]);
+
+/* Setup Layout Part - Footer */
+MetronicApp.controller('FooterController', ['$scope', function($scope) {
+    $scope.$on('$includeContentLoaded', function() {
+        Layout.initFooter(); // init footer
+    });
+}]);
 
 
 /* Setup Rounting For All Pages */
